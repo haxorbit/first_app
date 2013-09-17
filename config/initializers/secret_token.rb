913cb3f12c6cf59efb7e951460b71e472b52204b
Haxorbit <cedric@vintedge.com>
2013-09-17 05:50:12 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsTutorial::Application.config.secret_key_base = '8365d6f84f91fbe7cf9ead2f2530e52f4028eb55b681fd2a3e58362ba6d18736fde59f1f3b8c443b9bafbae1ef44d9a989f3c2dc7e06c647962da94431f58551'
---
+++ vendor/bundle/ruby/2.0.0/gems/json-1.8.0/ext/json/ext/generator/siteconf20130912-14953-71swqd.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130912-14953-1cd8dhz"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
